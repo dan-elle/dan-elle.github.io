d9d54f64e0dfb2f85be62dbe6f840c3a5ac80b13
dan-elle <danelledbd@gmail.com>
2024-05-16 01:20:16 -0400

    Added a CPU player to TicTacToe
    Very simple CPU logic, player is still likely to win but now you can play alone

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,9 +1,23 @@
 // constants for all of the parts of the game board
+//creating an array of cells
 const cells = document.querySelectorAll('[data-cell]')
+const cellsArray = Array.from(cells);
+const cellRow1 = cellsArray.slice(0, 3)
+const cellRow2 = cellsArray.slice(3, 6)
+const cellRow3 = cellsArray.slice(6, 9)
+const boardArray = [cellRow1, cellRow2, cellRow3]
+
 const board = document.getElementById('board')
+
+//winning page
 const winningMessageTextElement = document.querySelector('[data-win-message-text]')
 const winningMessageElement = document.getElementById('winMessage')
 const restartButton = document.getElementById('restartButton')
+
+// buttons for piece choices at game start
+const xButton = document.getElementById('xButton')
+const oButton = document.getElementById('oButton')
+const playerPieceChoice = document.getElementById('playerPieceChoice')
 
 // variables
 const xClass = 'x'
@@ -20,6 +34,10 @@ const winningCombos = [
 ]
 let oTurn
 
+let playerPiece
+let playerTurn
+let computerPiece
+
 // start the game
 start()
 
@@ -28,16 +46,40 @@ restartButton.addEventListener('click', start)
 
 //functions
 
-//reset all the variables upon starting the game to create a clean gameboard
+
 function start() {
-    winningMessageElement.classList.remove('show')
+    //reset all the variables upon starting the game to create a clean gameboard
     oTurn = false
+    winningMessageElement.classList.remove('show')
+    playerPieceChoice.classList.add('show')
     cells.forEach(cell => {
         cell.classList.remove(xClass)
         cell.classList.remove(oClass)
         cell.removeEventListener('click', handleClick)
         cell.addEventListener('click', handleClick, { once: true })
     })
+    xButton.addEventListener('click', chooseX)
+    oButton.addEventListener('click', chooseO)
+}
+
+//choosing piece functions
+function chooseX() {
+    playerPiece = xClass
+    computerPiece = oClass
+    playerTurn = true
+    playerPieceChoice.classList.remove('show')
+    setBoardHover()
+}
+
+function chooseO() {
+    playerPiece = oClass
+    computerPiece = xClass
+    playerTurn = false
+    playerPieceChoice.classList.remove('show')
+    let computerMove = computerPlay()
+    console.log("playing at", computerMove);
+    placeMark(cells[computerMove], computerPiece)
+    swapTurns()
     setBoardHover()
 }
 
@@ -52,6 +94,18 @@ function handleClick(event) {
         endGame(true)
     } else {
         swapTurns()
+        if (!playerTurn) {
+            let computerMove = computerPlay()
+            console.log("playing at", computerMove);
+            placeMark(cells[computerMove], computerPiece)
+            if (checkWin(computerPiece)) {
+                endGame(false)
+            } else if (isDraw()) {
+                endGame(true)
+            } else {
+                swapTurns()
+            }
+        }
         setBoardHover()
     }
 }
@@ -64,16 +118,19 @@ function placeMark(cell, currentClass) {
 //switch turns each round
 function swapTurns() {
     oTurn = !oTurn
+    playerTurn = !playerTurn
 }
 
 //show the player when hovering over a valid cell
 function setBoardHover() {
     board.classList.remove(xClass)
     board.classList.remove(oClass)
-    if (oTurn) {
-        board.classList.add(oClass)
-    } else {
-        board.classList.add(xClass)
+    if (playerTurn) {
+        if (oTurn) {
+            board.classList.add(oClass)
+        } else {
+            board.classList.add(xClass)
+        }
     }
 }
 
@@ -102,4 +159,42 @@ function endGame(draw) {
         winningMessageTextElement.innerText = `${oTurn ? "O's" : "X's"} Wins!`
     }
     winningMessageElement.classList.add('show')
+}
+
+function computerPlay() {
+    console.log("Computing move...");
+    let currentBestPlace
+    let currentBestLength = 0
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            console.log("Loop iteration:", i, j);
+            if (cells[i * 3 + j].classList.contains(computerPiece)) {
+                //move directionally through the board
+                for (let deltaX = -1; deltaX <= 1; deltaX += 1) {
+                    for (let deltaY = -1; deltaY <= 1; deltaY += 1) {
+                        let tempi = i + deltaX
+                        let tempj = j + deltaY
+                        //piece out of bounds or not moved
+                        if (tempi < 0 || tempi > 2 || tempj < 0 || tempj > 2 || (deltaX === 0 && deltaY === 0)) {
+                            continue
+                        }
+                        //check if the adjacent spot has computer piece
+                        if (cells[tempi * 3 + tempj].classList.contains(computerPiece)) {
+                            if (!cells[tempi * 3 + tempj + 1].classList.contains(xClass) && !cells[tempi * 3 + tempj + 1].classList.contains(oClass)) {
+                                console.log("Found computer piece at:", i, j);
+                                return tempi * 3 + tempj + 1
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //if not returned at this point, no winning move exists, play random move
+    let randomMove = Math.floor(Math.random() * 9)
+    while (cells[randomMove].classList.contains(xClass) || cells[randomMove].classList.contains(oClass)) {
+        randomMove = Math.floor(Math.random() * 9)
+    }
+    console.log("didn't find winning move, playing random move:", randomMove);
+    return randomMove
 }
